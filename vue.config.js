d9c521c92e87738abc115676d65d04f0c8a59031
toBeOfUse <mitchjacov@gmail.com>
2023-01-05 19:49:22 -0500

    ??? how was this broken

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/catullus/" : "/",
-  outputDir: process.env.OUTPUT_DIR || path.resolve(__dirname, "./dist")
+  outputDir:
+    process.env.OUTPUT_DIR || require("path").resolve(__dirname, "./dist"),
 };
