9156d8f41f2c86ea13223500f6f380a2db362bab
Mitch J <mjacovet@kent.edu>
2025-06-08 23:32:50 -0400

    convert to a vite project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-    <Poetry />
+  <Poetry />
 </template>
 
 <script>
 import Poetry from "./components/Poetry.vue";
 export default {
-    name: "App",
-    components: {
-        Poetry
-    }
+  name: "App",
+  components: {
+    Poetry,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/catullus/" : "/",
-  outputDir:
-    process.env.OUTPUT_DIR || require("path").resolve(__dirname, "./dist"),
-};
