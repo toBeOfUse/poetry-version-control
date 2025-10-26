eddfbc2d585f7c953b249a4cdc5889f04f72fe49
Mitch J <mjacovet@kent.edu>
2025-10-26 15:11:30 -0400

    fix build more

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   root: "src",
+  publicDir: '../public/',
   build: {
+    copyPublicDir: true,
     outDir: "../dist",
     emptyOutDir: true,
   },
